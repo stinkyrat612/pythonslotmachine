d2ab34cc0ac51d2f59864aa599d8f9da77f2b3f7
stinkyrat612 <xxshadowdeveloperxx@gmail.com>
2025-02-27 19:34:06 +0100

    Fixed savefiles and added a return option in a specific menu.

---
+++ slot_machine/debug.py
@@ -0,0 +1,17 @@
+# Functions used for debugging maingame.py:
+
+def DEBUG_ADDCASH():
+    # I only slide this function in for testing, don't abuse please.
+    global user_balance
+    user_balance += 1000
+
+
+def DEBUG_VARIABLEDUMP():
+    globals_dump = globals()
+    for var_name, value in globals_dump.items():
+        print(f"{var_name}: {value}")
+
+
+if __name__ == "__main__":
+    print("Do not run this program directly, it's a library for maingame.py")
+    exit()
